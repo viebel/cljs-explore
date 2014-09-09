d00c64d34abc21b0656ba62edb2daaa1e5208115
Yehonathan Sharvit <viebel@gmail.com>
2014-09-09 12:38:31 +0300

    cleanup

--- src/cljs_explore/me.cljs
+++ src/cljs_explore/me.cljs
@@ -1,5 +1,4 @@
 (ns user
-  (:require-macros [cljs_explore.macros :as m])
   (:require [clojure.string :as string]
             [goog.userAgent.product :as product]
             [cljs.reader :as reader]))
@@ -8,7 +7,8 @@
 (js/alert "aaba")
 (defn console [s] (.log js/console s))
 
-(m/deftry moo [a] (print "aaaaa"))
-
 (defn ^:export foo [] :foo)
+
+(defn foo [])
+(foo)
 
