028c389ce3c6fecc6f3321dcfaa37f4c86697680
Yehonathan Sharvit <viebel@Yehonathans-MBP.home>
2015-01-23 00:07:03 +0200

    purnam compilation warning

--- project.clj
+++ project.clj
@@ -6,11 +6,12 @@
                  [shodan "0.3.0"]
                  [com.cemerick/pprng "0.0.2"]
                  [cljs-http "0.1.14"]
+                 [im.chit/purnam "0.5.1"]
                  [com.cemerick/piggieback "0.1.3"]
                  [net.polyc0l0r/konserve "0.1.0"]
                  [com.andrewmcveigh/cljs-time "0.1.4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/clojurescript "0.0-2280"]]
+                 [org.clojure/clojurescript "0.0-2411"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
--- src/cljs_explore/me.cljs
+++ src/cljs_explore/me.cljs
@@ -1,9 +1,14 @@
 (ns user
+  (:use-macros [purnam.core :only [? ! obj !>]])
   (:require [clojure.string :as string]
             [cemerick.pprng :as rng]
             [cljs.reader :as reader]))
 
 
-(def rng (rng/rng))
-(js/alert 
-  (rng/int rng))
+(set! js/aa #js {})
+(def a js/aa)
+(! a.b.c 5555)
+(+ 2 (? a.b.c))
+(+ 2  (-> js/aa
+        (aget "b")
+        (aget "c")))
